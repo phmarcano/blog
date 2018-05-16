b70cf923fdce8ac2320cd9dfe8386bdcedecbe9f
Pedro Hernandez <pedro.hernandez@globant.com>
2018-05-16 00:47:19 -0300

    Add backend configuration

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,9 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     MediaMonks\RestApiBundle\MediaMonksRestApiBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
