51a5b4a73a32f1bb54f8905c9efbb478dadefc1f
Pedro Hernandez <pedro.hernandez@globant.com>
2018-05-18 00:44:43 -0300

    Add posts list and post datail

---
+++ src/Controller/PostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/post")
+ */
+class PostController extends Controller
+{
+    /**
+     * @Route("/", name="post_index", methods="GET")
+     */
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', ['posts' => $postRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/{id}", name="post_show", methods="GET")
+     */
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', ['post' => $post]);
+    }
+}
